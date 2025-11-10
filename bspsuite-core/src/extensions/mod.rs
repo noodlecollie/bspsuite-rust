f8f74d23ec4a95d6534e9945527bfc82cb9e15d8
Vesper <3692034+noodlecollie@users.noreply.github.com>
2025-11-10 18:03:16 +0000

    Improved things

--- bspsuite-core/src/extensions/mod.rs
+++ bspsuite-core/src/extensions/mod.rs
@@ -3,33 +3,58 @@ use std::fs::ReadDir;
 use std::path::{Path, PathBuf};
 
 use anyhow::{Context, Result, bail};
+#[cfg(target_os = "linux")]
+use libloading::os::linux::Symbol as UnsafeSymbol;
+#[cfg(target_os = "windows")]
+use libloading::os::windows::Symbol as UnsafeSymbol;
 use libloading::{Library, Symbol, library_filename};
 use target_lexicon::{HOST, OperatingSystem};
 
 const BSPSUITE_EXT_INTERFACE_CURRENT_VERSION: usize = 1;
 const BSPSUITE_EXT_SYM_GETINTERFACEVERSION: &[u8] = b"bspsuite_ext_get_interface_version";
-type ExtFnGetInterfaceVersion = unsafe extern "C" fn() -> usize;
+type ExtFnGetInterfaceVersion = extern "C" fn() -> usize;
 
 const BSPSUITE_EXT_SYM_PRESENT_SERVICES: &[u8] = b"bspsuite_ext_present_services";
-type ExtFnPresentServices = unsafe extern "C" fn(&ExtensionServicesApi) -> ExtensionServicesResult;
+type ExtFnPresentServices = extern "C" fn(&ExtensionServicesApi) -> ExtensionServicesResult;
 
 pub struct Extension
 {
 	library: Library,
+	present_services_symbol: UnsafeSymbol<ExtFnPresentServices>,
 }
 
 impl Extension
 {
-	pub fn new(library: Library) -> Extension
+	pub fn from(library: Library) -> Result<Extension>
 	{
-		return Extension { library: library };
+		// Unsafe symbol calls are allowed here, since all the symbols are stored
+		// privately on the struct, and the struct cannot live longer than the library.
+		let present_servives_symbol: UnsafeSymbol<ExtFnPresentServices> =
+			unsafe { get_unsafe_symbol(&library, BSPSUITE_EXT_SYM_PRESENT_SERVICES)? };
+
+		return Ok(Extension {
+			library: library,
+			present_services_symbol: present_servives_symbol,
+		});
+	}
+
+	pub fn present_services(&self, api: &ExtensionServicesApi) -> ExtensionServicesResult
+	{
+		return (*self.present_services_symbol)(api);
 	}
 }
 
 #[repr(C)]
+/// Result codes that may be returned by an extension from a call to
+/// bspsuite_ext_present_services.
 pub enum ExtensionServicesResult
 {
+	/// The extension was able to obtain pointers to all the services it
+	/// required.
 	Ok,
+
+	/// The extension was unable to obtain pointers to one or more services that
+	/// it required.
 	Missed,
 }
 
@@ -76,10 +101,9 @@ pub fn find_extensions(root: &Path) -> Result<Vec<PathBuf>>
 pub fn load_extension<'lib>(path: &PathBuf) -> Result<Extension>
 {
 	let library: Library = unsafe { Library::new(library_filename(path.as_os_str())) }?;
-	// let present_services: Symbol<'lib, ExtFnPresentServices> =
-	// 	get_interface_to_extension(&library)?;
+	check_extension_interface_version(&library)?;
 
-	return Ok(Extension::new(library));
+	return Extension::from(library);
 }
 
 pub fn load_extensions(paths: &Vec<PathBuf>) -> Vec<Result<Extension>>
@@ -97,14 +121,12 @@ const fn library_extension_for_platform() -> &'static str
 	};
 }
 
-fn get_interface_to_extension<'lib>(
-	library: &'lib Library,
-) -> Result<Symbol<'lib, ExtFnPresentServices>>
+fn check_extension_interface_version(library: &Library) -> Result<()>
 {
-	let get_interface_version: Symbol<'lib, ExtFnGetInterfaceVersion> =
+	let get_interface_version: Symbol<ExtFnGetInterfaceVersion> =
 		unsafe { library.get(BSPSUITE_EXT_SYM_GETINTERFACEVERSION) }?;
 
-	let interface_version: usize = unsafe { get_interface_version() };
+	let interface_version: usize = get_interface_version();
 
 	if interface_version != BSPSUITE_EXT_INTERFACE_CURRENT_VERSION
 	{
@@ -114,8 +136,13 @@ fn get_interface_to_extension<'lib>(
 		);
 	}
 
-	let present_services: Symbol<'lib, ExtFnPresentServices> =
-		unsafe { library.get(BSPSUITE_EXT_SYM_PRESENT_SERVICES) }?;
+	Ok(())
+}
 
-	return Ok(present_services);
+// It is the caller's responsibility that the symbol is not used after the
+// library is unloaded.
+unsafe fn get_unsafe_symbol<T>(library: &Library, name: &[u8]) -> Result<UnsafeSymbol<T>>
+{
+	let symbol: Symbol<T> = unsafe { library.get(name) }?;
+	return unsafe { Ok(symbol.into_raw()) };
 }
