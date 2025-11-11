a989b26706f24e032bcf7ad4be15cff4d4eee9d0
Vesper <3692034+noodlecollie@users.noreply.github.com>
2025-11-11 09:31:23 +0000

    Created dist directory structure

--- bspsuite-buildtasks/src/main.rs
+++ bspsuite-buildtasks/src/main.rs
@@ -10,7 +10,6 @@ use anyhow::Context;
 use clap::Parser;
 use glob;
 use glob::Paths;
-use glob::PatternError;
 use target_lexicon::{HOST, OperatingSystem};
 
 // A lot of code in this file is based off
@@ -67,58 +66,88 @@ fn run_build_command() -> Result<(), DynError>
 	let src_dir: PathBuf = binaries_dir();
 	let dist_dir: PathBuf = src_dir.join("dist");
 
-	if dist_dir.is_file()
-	{
-		Err(format!(
-			"Dist directory {} is actually a file",
-			dist_dir.to_str().unwrap()
-		))?;
-	}
+	create_dist_dir(&dist_dir)?;
 
-	if !dist_dir.is_dir()
-	{
-		std::fs::create_dir(dist_dir.clone()).with_context(|| {
-			format!(
-				"Failed to create dist directory {}",
-				dist_dir.to_str().unwrap()
-			)
-		})?;
-	}
+	let lib_prefix: &str = library_prefix_for_platform();
+	let lib_ext: &str = library_extension_for_platform();
+	let exe_ext: &str = executable_extension_for_platform();
 
-	copy_files(&src_dir, &dist_dir, library_extension_for_platform())?;
+	copy_file(&src_dir, &dist_dir, format!("bspc{exe_ext}").as_str())?;
+	copy_file(
+		&src_dir,
+		&dist_dir,
+		format!("{lib_prefix}bspcore{lib_ext}").as_str(),
+	)?;
+
+	let glob_str: String = format!("{lib_prefix}*ext{lib_ext}");
+	copy_glob(&src_dir, &dist_dir.join("extensions"), glob_str.as_str())?;
 
 	Ok(())
 }
 
-fn copy_files(src: &PathBuf, dest: &PathBuf, extension: &str) -> Result<(), DynError>
+fn create_dist_dir(dist_dir: &PathBuf) -> Result<(), DynError>
 {
-	if !dest.is_dir()
+	create_dir(&dist_dir)?;
+	create_dir(&dist_dir.join("games"))?;
+	create_dir(&dist_dir.join("extensions"))?;
+
+	Ok(())
+}
+
+fn create_dir(dir: &PathBuf) -> Result<(), DynError>
+{
+	if dir.is_file()
 	{
 		Err(format!(
-			"Dest directory {} does not exist",
-			dest.to_str().unwrap()
+			"Failed to create directory: {} is actually a file",
+			dir.to_str().unwrap()
 		))?;
 	}
 
-	let glob_str: String = format!("{}/*{extension}", src.to_str().unwrap());
-	let glob_result: Paths = glob::glob(glob_str.as_str()).unwrap();
+	if !dir.is_dir()
+	{
+		std::fs::create_dir(dir.clone())
+			.with_context(|| format!("Failed to create directory {}", dir.to_str().unwrap()))?;
+	}
+
+	Ok(())
+}
+
+fn copy_glob(src: &PathBuf, dest: &PathBuf, glob_str: &str) -> Result<(), DynError>
+{
+	let full_glob_str: String = format!("{}/{glob_str}", src.to_str().unwrap());
+	let glob_result: Paths = glob::glob(full_glob_str.as_str()).unwrap();
 
 	for source_file in glob_result
 	{
-		let file_name: String =
-			String::from(source_file.unwrap().file_name().unwrap().to_str().unwrap());
-
-		let source_path: PathBuf = src.join(file_name.clone());
-		let dest_path: PathBuf = dest.join(file_name.clone());
-
-		println!(
-			"Copying {} to {}",
-			source_path.to_str().unwrap(),
-			dest_path.to_str().unwrap()
-		);
-
-		std::fs::copy(source_path, dest_path)?;
+		copy_file(
+			src,
+			dest,
+			source_file.unwrap().file_name().unwrap().to_str().unwrap(),
+		)?;
 	}
+
+	Ok(())
+}
+
+fn copy_file(src: &PathBuf, dest: &PathBuf, name: &str) -> Result<(), DynError>
+{
+	let source_path: PathBuf = src.join(name);
+	let dest_path: PathBuf = dest.join(name);
+
+	if source_path == dest_path
+	{
+		return Ok(());
+	}
+
+	println!(
+		"Copying: {} -> {}",
+		source_path.to_str().unwrap(),
+		dest_path.to_str().unwrap()
+	);
+
+	std::fs::copy(&source_path, &dest_path)
+		.with_context(|| format!("Failed to copy {}", source_path.to_str().unwrap()))?;
 
 	Ok(())
 }
@@ -147,6 +176,16 @@ const fn library_extension_for_platform() -> &'static str
 	{
 		OperatingSystem::Windows => ".dll",
 		OperatingSystem::Linux => ".so",
+		_ => panic!("Unsupported operating system"),
+	};
+}
+
+const fn library_prefix_for_platform() -> &'static str
+{
+	return match HOST.operating_system
+	{
+		OperatingSystem::Windows => "",
+		OperatingSystem::Linux => "lib",
 		_ => panic!("Unsupported operating system"),
 	};
 }
